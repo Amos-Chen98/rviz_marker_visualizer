af14bfc852ad83debcfd0222893af469b59a930c
Yicheng <yicheng-chen@outlook.com>
2025-01-17 14:06:45 +0900

    fix: update RViz configuration and visualizer node for improved marker handling

--- scripts/visualizer_node.py
+++ scripts/visualizer_node.py
@@ -10,8 +10,11 @@ class Visualizer:
         rospy.init_node(node_name)
 
         # init self.pose_array a an empty list
-        self.pose_array = []
         self.marker_type = rospy.get_param('~marker_type', 'sphere')
+        self.marker_array = MarkerArray()
+        self.marker_id = 0
+        self.current_pose = None
+        self.last_pose = None
 
         # Subscriber
         self.pose_sub = rospy.Subscriber('/pose_topic', PoseStamped, self.pose_cb)
@@ -20,9 +23,10 @@ class Visualizer:
         self.marker_pub = rospy.Publisher('/marker_array', MarkerArray, queue_size=10)
 
     def pose_cb(self, data):
-        self.pose_array.append(np.array([data.pose.position.x,
-                                         data.pose.position.y,
-                                         data.pose.position.z]))
+        if self.current_pose is not None:
+            self.last_pose = self.current_pose
+        self.current_pose = data.pose
+
         if self.marker_type == 'line':
             self.publish_continuous_markers()
         else:
@@ -32,69 +36,72 @@ class Visualizer:
         '''
         Doc: https://wiki.ros.org/rviz/DisplayTypes/Marker 
         '''
-        marker_array = MarkerArray()
+        if self.last_pose is None:
+            return
 
-        for i in range(len(self.pose_array)-1):
-            marker = Marker()
-            marker.header.frame_id = "map"
-            marker.header.seq = i
-            marker.header.stamp = rospy.Time.now()
-            marker.id = i
-            marker.type = Marker.LINE_STRIP
-            marker.action = Marker.ADD
+        marker = Marker()
+        marker.header.frame_id = "world"
+        marker.header.seq = self.marker_id
+        marker.id = self.marker_id
+        marker.header.stamp = rospy.Time.now()
+        marker.type = Marker.LINE_STRIP
+        marker.action = Marker.ADD
 
-            line_head = [self.pose_array[i][0], self.pose_array[i][1], self.pose_array[i][2]]
-            line_tail = [self.pose_array[i+1][0], self.pose_array[i+1][1], self.pose_array[i+1][2]]
-            marker.points = [Point(line_head[0], line_head[1], line_head[2]),
-                             Point(line_tail[0], line_tail[1], line_tail[2])]
+        marker.points = [Point(self.last_pose.position.x,
+                               self.last_pose.position.y,
+                               self.last_pose.position.z),
+                         Point(self.current_pose.position.x,
+                               self.current_pose.position.y,
+                               self.current_pose.position.z)]
 
-            marker.pose.orientation.w = 1.0
+        marker.pose.orientation.w = 1.0
 
-            marker.scale.x = 0.3
+        marker.scale.x = 0.1
 
-            marker.color.r = 1.0
-            marker.color.g = 0.0
-            marker.color.b = 0.0
-            marker.color.a = 1.0
+        marker.color.r = 1.0
+        marker.color.g = 0.0
+        marker.color.b = 0.0
+        marker.color.a = 1.0
 
-            # Add the marker to the MarkerArray
-            marker_array.markers.append(marker)
+        # Add the marker to the MarkerArray
+        self.marker_array.markers.append(marker)
 
         # Publish the MarkerArray
-        self.marker_pub.publish(marker_array)
+        self.marker_pub.publish(self.marker_array)
+
+        self.marker_id += 1
 
     def publish_discrete_markers(self):
-        marker_array = MarkerArray()
+        marker = Marker()
+        marker.header.frame_id = "world"
+        marker.header.seq = self.marker_id
+        marker.id = self.marker_id
+        marker.header.stamp = rospy.Time.now()
+        marker.type = Marker.SPHERE
+        marker.action = Marker.ADD
 
-        for i in range(len(self.pose_array)):
-            marker = Marker()
-            marker.header.frame_id = "map"
-            marker.header.seq = i
-            marker.header.stamp = rospy.Time.now()
-            marker.id = i
-            marker.type = Marker.SPHERE
-            marker.action = Marker.ADD
+        marker.pose.position.x = self.current_pose.position.x
+        marker.pose.position.y = self.current_pose.position.y
+        marker.pose.position.z = self.current_pose.position.z
 
-            marker.pose.position.x = self.pose_array[i][0]
-            marker.pose.position.y = self.pose_array[i][1]
-            marker.pose.position.z = self.pose_array[i][2]
+        marker.pose.orientation.w = 1.0
 
-            marker.pose.orientation.w = 1.0
+        marker.scale.x = 0.1
+        marker.scale.y = 0.1
+        marker.scale.z = 0.1
 
-            marker.scale.x = 0.3
-            marker.scale.y = 0.3
-            marker.scale.z = 0.3
+        marker.color.r = 0.0
+        marker.color.g = 1.0
+        marker.color.b = 0.0
+        marker.color.a = 1.0
 
-            marker.color.r = 0.0
-            marker.color.g = 1.0
-            marker.color.b = 0.0
-            marker.color.a = 1.0
-
-            # Add the marker to the MarkerArray
-            marker_array.markers.append(marker)
+        # Add the marker to the MarkerArray
+        self.marker_array.markers.append(marker)
 
         # Publish the MarkerArray
-        self.marker_pub.publish(marker_array)
+        self.marker_pub.publish(self.marker_array)
+
+        self.marker_id += 1
 
 
 if __name__ == '__main__':
